99c13b8b489b58803ac2b524deeace34722e9b48
Zhang <315161505@qq.com>
2017-09-23 16:10:23 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/pengpeng/MainActivity.java
+++ app/src/main/java/com/example/pengpeng/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.pengpeng;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button chooseAreaButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chooseAreaButton=(Button) findViewById(R.id.chooseArea_Button);
+        chooseAreaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,AreaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }

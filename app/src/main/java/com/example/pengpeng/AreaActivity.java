7f4db575fae688d0d699c879ae4017c33a7b49a9
Zhang <315161505@qq.com>
2017-09-24 21:19:44 +0800

    加入显示天气信息及获取必应每日一图的功能

--- app/src/main/java/com/example/pengpeng/AreaActivity.java
+++ app/src/main/java/com/example/pengpeng/AreaActivity.java
@@ -1,7 +1,13 @@
 package com.example.pengpeng;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.pengpeng.gson.Weather;
+import com.example.pengpeng.util.Utility;
 
 public class AreaActivity extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class AreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherString=prefs.getString("weather",null);
+      if(weatherString!=null){
+            Weather weather= Utility.handleWeatherResponse(weatherString);
+            String huancunId=weather.basic.weatherId;
+            Intent intent=new Intent(this,WeatherActivity.class);
+            intent.putExtra("weather_id",huancunId);
+            startActivity(intent);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/example/pengpeng/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.pengpeng.gson;
+
+/**
+ * Created by Administrator on 2017/9/23 0023.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
